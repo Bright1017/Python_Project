c8b0f3b53b90a3f9bf7fb6d8dac43d71bdc49991
Bright1017 <melvinbright101@gmail.com>
2025-06-16 14:54:35 +0100

    PYTHON CLASS

--- python_beginner101.py
+++ python_beginner101.py
@@ -7,32 +7,86 @@
 """Goal:
 Create a simple Python script to track a single customer's order in your delivery app."""
 
-# Customer order summary
-customer_name = "bright"
-pickup_address = "12 Omodara Ave"
-delivery_address = "13 Fagba Road"
-item_description = "iphone"
-quantity = 10
-delivery_fee = 59.99
-price_per_item = 2000
-is_paid = False
-rider_name = "chika"
-rider_number = 123456789
-if is_paid:
-    print("Payment Status: ✅ Paid")
-else:
-    print("Payment Status: ❌ Awaiting payment")
+# # Customer order summary
+# customer_name = "bright"
+# pickup_address = "12 Omodara Ave"
+# delivery_address = "13 Fagba Road"
+# item_description = "iphone"
+# quantity = 10
+# delivery_fee = 59.99
+# price_per_item = 2000
+# is_paid = False
+# rider_name = "chika"
+# rider_number = 123456789
+# if is_paid:
+#     print("Payment Status: ✅ Paid")
+# else:
+#     print("Payment Status: ❌ Awaiting payment")
 
-# Calculating total cost
-total_cost = quantity * price_per_item
-final_amount_due = total_cost + delivery_fee
+# # Calculating total cost
+# total_cost = quantity * price_per_item
+# final_amount_due = total_cost + delivery_fee
 
-# Order Summary
-print("Order Summary")
-print("Customer Name:", customer_name)
-print("Pickup Address:", pickup_address)
-print(f"Rider Name/Cell Number:", rider_name, rider_number)
-print("Delivery Address:", delivery_address)
-print("Item:", item_description)
-print(f"Total Cost: ${total_cost}")
-print(f"Here's your total cost: ${final_amount_due}")
+# # Order Summary
+# print("Order Summary")
+# print("Customer Name:", customer_name)
+# print("Pickup Address:", pickup_address)
+# print(f"Rider Name/Cell Number:", rider_name, rider_number)
+# print("Delivery Address:", delivery_address)
+# print("Item:", item_description)
+# print(f"Total Cost: ${total_cost}")
+# print(f"Here's your total cost: ${final_amount_due}")
+
+# """
+# Task:
+# Create a variable for user_age.
+
+# Use if statements to:
+
+# Print “You’re eligible to register” if age is 18 or above.
+
+# Else print “You’re too young to register.”
+# """
+
+# # Ask users for their age
+# age = int(input("What is your age? "))
+
+# # check if user is eligible to register based on age
+# if age >= 18:
+#     print("You're eligible to register")
+# else:
+#      print("You're not eligible to register")
+
+
+# x = "bright" # This a global variable which can reused anywhere in our code
+
+# def myfunc():
+#     x = "chika" # This is a local variable 
+#     print(x + " is learning phython")
+
+# myfunc()
+# print(x + " is learning phython")
+
+# DATA TYPES IN PYTHON/ 3 NUMERIC NUMBER
+
+# bright = 20 # int
+# bright1= 20.6 # float
+# Bright = 2j # complex
+# print(type(bright))
+# print(type(bright1))
+# print(type(Bright))
+
+# DATA TYPE CONVERSION
+# age = 20 # int
+# price = 20.6 # float
+# imaginary_unit = 76j # complex
+
+# A = float(age) # CONVERT FROM INT TO FLOAT
+# B = int(price) # CONVERT FROM FLOAT TO INT
+# C = complex(age) # CONVERT FROM INT TO COMPLEX
+# E = str(imaginary_unit)
+# print("A:", A, type(A))
+# print("B:", B, type(B))
+# print("C:", C, type(C))
+# print("E:", E, type(E))
+
